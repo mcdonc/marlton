86f609098374c0db803da139ffcd1ec490c40510
Chris McDonough <chrism@plope.com>
2009-06-23 15:46:28 +0000

    Use 1.0 spellings.

--- bfgsite/scripts/debug.py
+++ bfgsite/scripts/debug.py
@@ -1,13 +1,9 @@
 """ Run an interactive debugging session  """
 
-from code import interact
 import os
 import sys
 
-from repoze.bfg.registry import registry_manager
-from repoze.bfg.interfaces import IRootFactory
-
-from paste.deploy import loadapp
+from repoze.bfg.paster import BFGShellCommand
 
 def main(argv=sys.argv):
     config = None
@@ -17,22 +13,12 @@ def main(argv=sys.argv):
         # we assume that the console script lives in the 'bin' dir of a
         # sandbox or buildout, and that the .ini file lives in the 'etc'
         # directory of the sandbox or buildout
-        me = sys.argv[0]
-        me = os.path.abspath(me)
-        sandbox = os.path.dirname(os.path.dirname(me))
+        exe = sys.executable
+        sandbox = os.path.dirname(os.path.dirname(os.path.abspath(exe)))
         config = os.path.join(sandbox, 'bfgsite.ini')
-
-    config = os.path.abspath(os.path.normpath(config))
-
-    app = loadapp('config:%s' % config, name='website')
-    registry_manager.push(app.registry)
-    root_factory = app.registry.getUtility(IRootFactory)
-    environ = {}
-    root = root_factory(environ)
-    cprt = ('Type "help" for more information. "root" is the bfgsite '
-            'root object.')
-    banner = "Python %s on %s\n%s" % (sys.version, sys.platform, cprt)
-    interact(banner, local={'root':root})
+    shell = BFGShellCommand('bfg')
+    shell.args = (config, 'website')
+    shell.command()
 
 if __name__ == '__main__':
     main()
