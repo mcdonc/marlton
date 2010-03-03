103d26131901dd8695d6c8fea1d6348b78b5ea53
Chris McDonough <chrism@plope.com>
2010-03-03 14:24:49 +0000

    Bleh.

--- bfgsite/views/captcha.py
+++ bfgsite/views/captcha.py
@@ -14,7 +14,8 @@ def captcha_jpeg(context, request):
     image = captcha.render()
     image.save(output, 'JPEG')
     data = output.getvalue()
-    session = site.sessions.get(request.environ.get('repoze.browserid'), {})
+    browserid = request.environ.get('repoze.browserid')
+    session = site.sessions.get(browserid)
     session['captcha_solutions'] = captcha.solutions
     r = Response(data, '200 OK', [ ('Content-Type', 'image/jpeg'),
                                    ('Content-Length', len(data)) ])
