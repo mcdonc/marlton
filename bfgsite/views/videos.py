b97121f119486f8ce0260e6257a0387b877a8497
Chris McDonough <chrism@plope.com>
2010-08-07 18:39:18 +0000

    add groundhog1-10

--- bfgsite/views/videos.py
+++ bfgsite/views/videos.py
@@ -6,12 +6,13 @@ from bfgsite.utils import API
 
 @bfg_view(for_=IWebSite, name='videos', permission='view',
           renderer='bfgsite.views:templates/videos.pt')
-@bfg_view(for_=IWebSite, name='groundhog2', permission='view',
-          renderer='bfgsite.views:templates/videos.pt')
-@bfg_view(for_=IWebSite, name='groundhog3', permission='view',
-          renderer='bfgsite.views:templates/videos.pt')
 def videos_view(context, request):
     return dict(
         api = API(context, request),
         )
 
+for x in range(1, 11):
+    # add groundhog1 - 10 urls
+    gh_view = bfg_view(for_=IWebSite, name='groundhog%s' % x, permission='view',
+                       renderer='bfgsite.views:templates/videos.pt')
+    videos_view = gh_view(videos_view)
